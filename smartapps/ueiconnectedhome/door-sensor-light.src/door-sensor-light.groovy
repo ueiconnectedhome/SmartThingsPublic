83230ada8228b23755c26204ef9e2c805ba2f3fe
Ces Uei <ueiconnectedhome@gmail.com>
2015-12-23 12:02:53 -0800

    fix indents

--- smartapps/ueiconnectedhome/door-sensor-light.src/door-sensor-light.groovy
+++ smartapps/ueiconnectedhome/door-sensor-light.src/door-sensor-light.groovy
@@ -11,74 +11,81 @@ definition(
 )
 
 preferences {
-section ("Allow external service to control these things...") {
-input "switches", "capability.switch", multiple: true, required: true
+  section ("Allow external service to control these things...") {
+    input "switches", "capability.switch", multiple: true, required: true
+  }
+  section("Monitor this door or window") {
+    input "contacts", "capability.contactSensor"
+  }
 }
-section("Monitor this door or window") {
-input "contacts", "capability.contactSensor"
-}
-}
+
 mappings {
-path("/switches") {
-action: [
-GET: "listSwitches"
-]
+  path("/switches") {
+    action: [
+      GET: "listSwitches"
+    ]
+  }
+
+  path("/contacts") {
+    action: [
+      GET: "listContactSensors"
+    ]
+  }
+
+  path("/switches/:command") {
+    action: [
+      GET: "updateSwitches"
+    ]
+  }
 }
-path("/contacts") {
-action: [
-GET: "listContactSensors"
-]
-}
-path("/switches/:command") {
-action: [
-GET: "updateSwitches"
-]
-}
-}
+
 // returns a list like
 // [[name: "kitchen lamp", value: "off"], [name: "bathroom", value: "on"]]
 def listSwitches() {
-def resp = []
-switches.each {
-resp << [name: it.displayName, value: it.currentValue("switch")]
-}
+  def resp = []
+  switches.each {
+    resp << [name: it.displayName, value: it.currentValue("switch")]
+  }
 return resp
 //render contentType: "application/javascript", data: "${params.callback}(${resp.encodeAsJSON()})"
+}
 
-}
 def listContactSensors() {
-def resp = []
-contacts.each {
-resp << [name: it.displayName, value: it.currentValue("contact")]
-}
+  def resp = []
+  contacts.each {
+    resp << [name: it.displayName, value: it.currentValue("contact")]
+  }
 return resp
 //render contentType: "application/javascript", data: "${params.callback}(${resp.encodeAsJSON()})"
 }
+
 void updateSwitches() {
-// use the built-in request object to get the command parameter
-def command = params.command
-def value = [switch: "on", hue: hueColor, saturation: saturation, level: level as Integer ?: 100]
-if (command) {
-// check that the switch supports the specified command
-// If not, return an error using httpError, providing a HTTP status code.
-switches.each {
-if (!it.hasCommand(command)) {
-httpError(501, "$command is not a valid command for all switches specified")
-}
-}
+  // use the built-in request object to get the command parameter
+  def command = params.command
+  def value = [switch: "on", hue: hueColor, saturation: saturation, level: level as Integer ?: 100]
+
+  if (command) {
+    // check that the switch supports the specified command
+    // If not, return an error using httpError, providing a HTTP status code.
+    switches.each {
+      if (!it.hasCommand(command)) {
+        httpError(501, "$command is not a valid command for all switches specified")
+      }
+    }
+
 // all switches have the comand
 // execute the command on all switches
 // (note we can do this on the array - the command will be invoked on every element
 //switches."$command"()
-if("on" == "$command") {
-switches.setHue(70)
-switches.setSaturation(100) // Set the color to something fancy
-switches.setLevel(100) // Make sure the light brightness is 100%
-switches.on() // Turn the bulb on when open (this method does not come directly from the colorControl capability)
-} else {
-switches.off() // Turn the bulb off when closed (this method does not come directly from the colorControl capability)
-}
-}
+    if("on" == "$command") {
+      switches.setHue(70)
+      switches.setSaturation(100) // Set the color to something fancy
+      switches.setLevel(100) // Make sure the light brightness is 100%
+      switches.on() // Turn the bulb on when open (this method does not come directly from the colorControl capability)
+    } else {
+      switches.off() // Turn the bulb off when closed (this method does not come directly from the colorControl capability)
+    }
+  }
 }
 
 def doorSensorHandler(evt) {
@@ -94,11 +101,15 @@ def doorSensorHandler(evt) {
   }
 }
 
-def installed() {
+def installed() { 
+
  log.debug "Installed with settings: ${settings}"
  subscribe(contacts, "contact", doorSensorHandler )
+ 
 }
 
 def updated() {
+
 subscribe(contacts, "contact", doorSensorHandler )
+
 }
